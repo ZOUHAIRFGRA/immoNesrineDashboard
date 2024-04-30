e95444a35c3d6111ed546d70b0baf9a45727194f
Zouhair Fouiguira <zouhairfgra@gmail.com>
2024-04-30 10:21:41 +0100

    responsiveness

--- frontend/src/components/projects/ProjectModal.jsx
+++ frontend/src/components/projects/ProjectModal.jsx
@@ -4,39 +4,21 @@ import Modal from 'react-modal';
 import ProjectForm from './ProjectForm';
 import { XCircleIcon } from '@heroicons/react/24/solid';
 
-const customStyles = {
-  overlay: {
-    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Semi-transparent black overlay
-  },
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-    width: '70%', // Adjust the width as needed
-    maxHeight: '80vh', // Set maximum height and enable scrolling
-    overflowY: 'auto', // Enable vertical scrolling
-    border: 'none', // Remove border from the content
-    borderRadius: '8px', // Add border-radius to the content
-    padding: '2em', // Remove padding to avoid extra space
-    boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)', // Add box shadow for depth
-  },
-};
+
 
 const ProjectModal = ({ isOpen, onClose, onSubmit, projectToEdit }) => {
   return (
     <Modal
-      isOpen={isOpen}
-      onRequestClose={onClose}
-      contentLabel="Project Modal"
-      style={customStyles}
-    >
-      <div className="bg-white rounded-lg overflow-hidden">
-        <button className="absolute top-4 right-4 text-gray-600 hover:text-gray-800" onClick={onClose}>
-          <XCircleIcon className="h-6 w-6" />
-        </button>
+        isOpen={isOpen}
+        onRequestClose={onClose}
+        contentLabel="Project Modal"
+        className="modal fixed inset-0 flex items-center justify-center"
+        overlayClassName="overlay fixed inset-0 bg-gray-800 bg-opacity-75"
+      >
+      <div className="bg-white p-8 rounded-lg shadow-lg relative">
+          <button className="absolute top-4 right-4 text-gray-600 hover:text-gray-800" onClick={onClose}>
+            <XCircleIcon className="h-6 w-6" />
+          </button>
         <ProjectForm onSubmit={onSubmit} projectToEdit={projectToEdit} />
       </div>
     </Modal>
